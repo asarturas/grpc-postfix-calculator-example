62936065a615f1826ed82b063c01cbae6dd0f5d5
Arturas Smorgun <arturas@spikerlabs.com>
2017-08-03 22:22:06 +0100

    Move stuff around in server (for easier usage)

--- src/main/scala/CalculatorServer.scala
+++ src/main/scala/CalculatorServer.scala
@@ -1,25 +1,24 @@
-import calculator.CalculatorGrpc
-import io.grpc.ServerBuilder
+import calculator.{CalculationRequest, CalculatorGrpc}
+import io.grpc.{ManagedChannelBuilder, ServerBuilder}
 
 import scala.concurrent.ExecutionContext
 
 object CalculatorServer extends App {
 
-  val server =ServerBuilder
+  val server = ServerBuilder
     .forPort(55155)
     .addService(CalculatorGrpc.bindService(CalculatorService, ExecutionContext.global))
     .build.start
 
-  server.awaitTermination()
+//  // sample async client
+//  import scala.concurrent.ExecutionContext.Implicits.global
+//  val client = CalculatorGrpc.stub(
+//    ManagedChannelBuilder.forAddress("localhost", 55155).usePlaintext(true).build
+//  )
+//  client.calculate(CalculationRequest(Seq("5", "1", "2", "+", "4", "*", "+", "3", "-"))).onComplete { result =>
+//    println(result)
+//  }
 
-  /*
-  // sample async client
-  val client = CalculatorGrpc.stub(
-    ManagedChannelBuilder.forAddress("localhost", 55155).usePlaintext(true).build
-  )
-  client.calculate(CalculationRequest(Seq("5", "1", "2", "+", "4", "*", "+", "3", "-"))).onComplete { result =>
-    println(result)
-  }
-  */
+  server.awaitTermination()
 
 }
