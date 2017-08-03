f41b6dc3d6418b626de49df7153d221c29703ece
Arturas Smorgun <arturas@spikerlabs.com>
2017-08-03 21:49:54 +0100

    Add server (and show sample client)

---
+++ src/main/scala/CalculatorServer.scala
@@ -0,0 +1,25 @@
+import calculator.CalculatorGrpc
+import io.grpc.ServerBuilder
+
+import scala.concurrent.ExecutionContext
+
+object CalculatorServer extends App {
+
+  val server =ServerBuilder
+    .forPort(55155)
+    .addService(CalculatorGrpc.bindService(CalculatorService, ExecutionContext.global))
+    .build.start
+
+  server.awaitTermination()
+
+  /*
+  // sample async client
+  val client = CalculatorGrpc.stub(
+    ManagedChannelBuilder.forAddress("localhost", 55155).usePlaintext(true).build
+  )
+  client.calculate(CalculationRequest(Seq("5", "1", "2", "+", "4", "*", "+", "3", "-"))).onComplete { result =>
+    println(result)
+  }
+  */
+
+}
